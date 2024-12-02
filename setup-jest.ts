2278515f805cab86836793ef655802460d3a3d5e
Krystian Grzelak <k.grzelak.dev@gmail.com>
2024-12-02 13:37:49 +0100

    chore: replace Karma/Jasmine by Jest as the test environment

---
+++ setup-jest.ts
@@ -0,0 +1,7 @@
+import '@testing-library/dom';
+import '@testing-library/jest-dom';
+import '@testing-library/user-event';
+
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+setupZoneTestEnv();
