a00a16f318d79a8dd3a4e46db7f81cf66ee26b1f
Krystian Grzelak <k.grzelak.dev@gmail.com>
2024-12-04 14:48:33 +0100

    test: add moduleNameMapper to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   cache: true,
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+  },
 };
