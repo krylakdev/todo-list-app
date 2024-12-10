69b9ffbdcf3e3a393f802116b0b6d81648905bee
Krystian Grzelak <k.grzelak.dev@gmail.com>
2024-12-10 12:05:14 +0100

    test: theme-switcher component

--- src/app/features/shared/theme-switcher/theme-switcher.component.spec.ts
+++ src/app/features/shared/theme-switcher/theme-switcher.component.spec.ts
@@ -1,22 +1,63 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { WritableSignal, signal } from '@angular/core';
+import { fireEvent, render, screen } from '@testing-library/angular';
+
+import { ThemeSwitcherService } from '@core/services';
 
 import { ThemeSwitcherComponent } from './theme-switcher.component';
 
-describe('ThemeSwitcherComponent', () => {
-  let component: ThemeSwitcherComponent;
-  let fixture: ComponentFixture<ThemeSwitcherComponent>;
+describe(ThemeSwitcherComponent.name, () => {
+  let mockThemeSwitcherService: Partial<ThemeSwitcherService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ThemeSwitcherComponent],
-    }).compileComponents();
+  beforeEach(() => {
+    const themeSignal: WritableSignal<'light' | 'dark'> = signal('light');
 
-    fixture = TestBed.createComponent(ThemeSwitcherComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    mockThemeSwitcherService = {
+      theme: themeSignal,
+      toggleTheme: jest.fn(),
+    };
   });
 
-  it('should create', () => {
+  it('should render the component', async () => {
+    const { fixture } = await render(ThemeSwitcherComponent);
+
+    const component: ThemeSwitcherComponent = fixture.componentInstance;
+
     expect(component).toBeTruthy();
+  });
+
+  it('should correctly bind isChecked signal for light theme', async () => {
+    mockThemeSwitcherService.theme!.set('light');
+
+    await render(ThemeSwitcherComponent, {
+      providers: [{ provide: ThemeSwitcherService, useValue: mockThemeSwitcherService }],
+    });
+
+    const checkbox: HTMLInputElement = screen.getByRole('checkbox');
+
+    expect(checkbox.checked).toBe(false);
+  });
+
+  it('should correctly bind isChecked signal for dark theme', async () => {
+    mockThemeSwitcherService.theme!.set('dark');
+
+    await render(ThemeSwitcherComponent, {
+      providers: [{ provide: ThemeSwitcherService, useValue: mockThemeSwitcherService }],
+    });
+
+    const checkbox: HTMLInputElement = screen.getByRole('checkbox');
+
+    expect(checkbox.checked).toBe(true);
+  });
+
+  it('should call toggleTheme on checkbox change', async () => {
+    await render(ThemeSwitcherComponent, {
+      providers: [{ provide: ThemeSwitcherService, useValue: mockThemeSwitcherService }],
+    });
+
+    const checkbox: HTMLInputElement = screen.getByRole('checkbox');
+
+    fireEvent.change(checkbox);
+
+    expect(mockThemeSwitcherService.toggleTheme).toHaveBeenCalledTimes(1);
   });
 });
