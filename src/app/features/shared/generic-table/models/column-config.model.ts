8b97d8b5d5c1958c4f7069ffdda3c09bc191a02f
Krystian Grzelak <k.grzelak.dev@gmail.com>
2024-12-11 20:24:22 +0100

    refactor: create separate type for ColumnType

--- src/app/features/shared/generic-table/models/column-config.model.ts
+++ src/app/features/shared/generic-table/models/column-config.model.ts
@@ -1,5 +1,8 @@
+export type ColumnType = 'text' | 'date';
+
 export interface ColumnConfig<T extends object> {
   label: string;
+  type: ColumnType;
   property: keyof T;
   computeValue?: (row: T) => string;
 }
