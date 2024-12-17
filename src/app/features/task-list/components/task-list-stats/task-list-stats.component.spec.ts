4ae67be01f50f7046cc178a506d2d74f3166a133
Krystian Grzelak <k.grzelak.dev@gmail.com>
2024-12-17 14:18:08 +0100

    test: task-list-stats component

--- src/app/features/task-list/components/task-list-stats/task-list-stats.component.spec.ts
+++ src/app/features/task-list/components/task-list-stats/task-list-stats.component.spec.ts
@@ -1,22 +1,45 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { WritableSignal, signal } from '@angular/core';
+import { render, screen } from '@testing-library/angular';
+
+import { TasksStore } from '@core/stores';
 
 import { TaskListStatsComponent } from './task-list-stats.component';
 
-describe('TaskListStatsComponent', () => {
-  let component: TaskListStatsComponent;
-  let fixture: ComponentFixture<TaskListStatsComponent>;
+class MockTasksStore {
+  tasksCountAll: WritableSignal<number> = signal(10);
+  tasksCountIsImportant: WritableSignal<number> = signal(3);
+  tasksCountUncompleted: WritableSignal<number> = signal(9);
+  tasksCountCompleted: WritableSignal<number> = signal(1);
+}
 
+describe(TaskListStatsComponent.name, () => {
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TaskListStatsComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(TaskListStatsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    await render(TaskListStatsComponent, {
+      providers: [{ provide: TasksStore, useClass: MockTasksStore }],
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display the total tasks count', () => {
+    const totalTasksElement = screen.getByAltText('icon of total tasks count');
+    expect(totalTasksElement).toBeInTheDocument();
+    expect(screen.getByText('10')).toBeInTheDocument();
+  });
+
+  it('should display the uncompleted tasks count', () => {
+    const uncompletedTasksElement = screen.getByAltText('icon of uncompleted tasks count');
+    expect(uncompletedTasksElement).toBeInTheDocument();
+    expect(screen.getByText('9')).toBeInTheDocument();
+  });
+
+  it('should display the completed tasks count', () => {
+    const completedTasksElement = screen.getByAltText('icon of completed tasks count');
+    expect(completedTasksElement).toBeInTheDocument();
+    expect(screen.getByText('1')).toBeInTheDocument();
+  });
+
+  it('should display the important tasks count', () => {
+    const importantTasksElement = screen.getByAltText('icon of important tasks count');
+    expect(importantTasksElement).toBeInTheDocument();
+    expect(screen.getByText('3')).toBeInTheDocument();
   });
 });
