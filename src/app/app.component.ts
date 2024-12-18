c5bd86ba15ce6f105386eae6bb59c549c834d1bd
Krystian Grzelak <k.grzelak.dev@gmail.com>
2024-12-18 10:34:42 +0100

    feat: add task-form component in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit, inject } from '@angular/core';
 
 import { ThemeSwitcherService } from '@core/services';
 import { HeaderPanelComponent } from '@features/header-panel';
+import { TaskFormComponent } from '@features/task-form';
 import { TaskListComponent } from '@features/task-list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TaskListComponent, HeaderPanelComponent],
+  imports: [TaskListComponent, HeaderPanelComponent, TaskFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
