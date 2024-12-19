8bd9fd4d41b99cf9689ef74b7e319c2d14ae9262
Krystian Grzelak <k.grzelak.dev@gmail.com>
2024-12-19 14:27:37 +0100

    refactor: remove unnecessary code

--- src/app/features/shared/form-controls/pipes/form-control-error.pipe.ts
+++ src/app/features/shared/form-controls/pipes/form-control-error.pipe.ts
@@ -8,34 +8,7 @@ import { formControlErrorMessages } from '../constants';
   standalone: true,
 })
 export class FormControlErrorPipe implements PipeTransform {
-  // transform(errors: ValidationErrors | null): unknown {
-  //   return errors
-  //     ? Object.entries(errors)
-  //         .map(([key, value]) =>
-  //           typeof value === 'string' && value.length > 0
-  //             ? value
-  //             : value === true && this.errorMessages[key]
-  //               ? this.errorMessages[key]
-  //               : this.errorMessages['unknown']
-  //         )
-  //         .join('. ')
-  //     : '';
-  // }
-
   transform(errors: ValidationErrors | null): string {
-    console.log(
-      errors
-        ? Object.entries(errors)
-            .map(([key, value]) =>
-              typeof value === 'string' && value.length > 0
-                ? value
-                : value === true && formControlErrorMessages[key]
-                  ? formControlErrorMessages[key]
-                  : formControlErrorMessages['unknown']
-            )
-            .join('. ')
-        : ''
-    );
     return errors
       ? Object.entries(errors)
           .map(([key, value]) =>
