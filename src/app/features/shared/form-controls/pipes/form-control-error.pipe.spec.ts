f8977844a8a771d64fa3e9f3824c7749dba704f0
Krystian Grzelak <k.grzelak.dev@gmail.com>
2024-12-19 14:32:20 +0100

    test: form-control-error pipe

--- src/app/features/shared/form-controls/pipes/form-control-error.pipe.spec.ts
+++ src/app/features/shared/form-controls/pipes/form-control-error.pipe.spec.ts
@@ -1,8 +1,53 @@
+import { ValidationErrors } from '@angular/forms';
+
+import { formControlErrorMessages } from '../constants';
 import { FormControlErrorPipe } from './form-control-error.pipe';
 
-describe('FormControlErrorPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormControlErrorPipe();
-    expect(pipe).toBeTruthy();
+describe(FormControlErrorPipe.name, () => {
+  let pipe: FormControlErrorPipe;
+
+  beforeEach(() => {
+    pipe = new FormControlErrorPipe();
+  });
+
+  it('should return an empty string if errors are null', () => {
+    const result = pipe.transform(null);
+    expect(result).toBe('');
+  });
+
+  it('should return the correct error message for a known key with boolean value', () => {
+    const errors: ValidationErrors = { required: true };
+    const result = pipe.transform(errors);
+    expect(result).toBe(formControlErrorMessages['required']);
+  });
+
+  it('should return the correct error message for a known key with string value', () => {
+    const errors: ValidationErrors = { customError: 'Custom error message' };
+    const result = pipe.transform(errors);
+    expect(result).toBe('Custom error message');
+  });
+
+  it('should return the unknown error message for an unknown key with boolean value', () => {
+    const errors: ValidationErrors = { unknownKey: true };
+    const result = pipe.transform(errors);
+    expect(result).toBe(formControlErrorMessages['unknown']);
+  });
+
+  it('should join multiple error messages with a dot and a space', () => {
+    const errors: ValidationErrors = {
+      required: true,
+      maxLength: true,
+      customError: 'Custom message',
+    };
+    const result = pipe.transform(errors);
+    expect(result).toBe(
+      `${formControlErrorMessages['required']}. ${formControlErrorMessages['maxLength']}. Custom message`
+    );
+  });
+
+  it('should return the unknown error message for unknown keys without specific value', () => {
+    const errors: ValidationErrors = { anotherUnknownKey: {} };
+    const result = pipe.transform(errors);
+    expect(result).toBe(formControlErrorMessages['unknown']);
   });
 });
