b7c5f9137922bf47b815e9850bed166b3ec957b0
Krystian Grzelak <k.grzelak.dev@gmail.com>
2024-12-19 14:24:24 +0100

    style: exclude :disabled state when button is active

--- src/app/features/task-form/task-form.component.ts
+++ src/app/features/task-form/task-form.component.ts
@@ -1,11 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+import { TextControlComponent } from '@features/shared';
+
+import { TaskForm } from './models';
+import { TaskFormAdapterService } from './services';
 
 @Component({
   selector: 'app-task-form',
   standalone: true,
-  imports: [],
+  imports: [TextControlComponent, ReactiveFormsModule],
+  providers: [TaskFormAdapterService],
   templateUrl: './task-form.component.html',
   styleUrl: './task-form.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TaskFormComponent {}
+export class TaskFormComponent implements OnInit {
+  readonly #taskFormAdapter = inject(TaskFormAdapterService);
+
+  form: FormGroup<TaskForm> = this.#taskFormAdapter.createForm();
+
+  ngOnInit(): void {
+    this.form.valueChanges.subscribe(console.log);
+  }
+
+  handleSubmitForm(): void {
+    if (this.form.invalid) return;
+
+    console.log('submit');
+  }
+}
